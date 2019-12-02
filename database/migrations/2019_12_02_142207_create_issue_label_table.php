dfe5857e587697db38ffe9172951034d99890d46
Earl Tacogdoy <earltacs96@gmail.com>
2019-12-02 22:37:05 +0800

    Migration: create issue_label

---
+++ database/migrations/2019_12_02_142207_create_issue_label_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssueLabelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issue_label', function (Blueprint $table) {
+            $table->bigIncrements('ilabel_id');
+            $table->Integer('label_id')->unsigned();
+            $table->Integer('issue_id')->unsigned();
+            $table->boolean('label_istask')->default(0);
+        });
+
+        Schema::table('issue_label', function (Blueprint $table){
+            $table->foreign('label_id')
+            ->references('label_id')
+            ->on('project_labels')
+            ->onUpdate('cascade')->onDelete('restrict');
+
+            $table->foreign('issue_id')
+            ->references('issue_id')
+            ->on('project_issues')
+            ->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issue_label');
+    }
+}
