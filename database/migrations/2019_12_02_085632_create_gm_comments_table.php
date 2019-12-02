1b5455b4a1eb6d260bb3904e80e9125d80aa9965
Earl Tacogdoy <earltacs96@gmail.com>
2019-12-02 22:36:00 +0800

    Migration: create gm_comments

---
+++ database/migrations/2019_12_02_085632_create_gm_comments_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGmCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gm_comments', function (Blueprint $table) {
+            $table->bigIncrements('gmc_id');
+            $table->text('gmc_desc');
+            $table->Integer('owner')->unsigned();
+            $table->bigInteger('gmc_target')->unsigned();
+            $table->timestamp('gmc_datecreted')->useCurrent();
+            $table->timestamp('gmc_dateupdated')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+
+        Schema::table('gm_comments', function (Blueprint $table){
+            $table->foreign('owner')
+            ->references('user_id')
+            ->on('user_accounts')
+            ->onUpdate('cascade')->onDelete('restrict');
+
+            $table->foreign('gmc_target')
+            ->references('message_id')
+            ->on('group_messages')
+            ->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gm_comments');
+    }
+}
