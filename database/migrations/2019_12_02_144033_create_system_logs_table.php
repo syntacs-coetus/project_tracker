fc339e6725b990beb3aacac50ca1fa26780b6d2d
Earl Tacogdoy <earltacs96@gmail.com>
2019-12-02 23:09:10 +0800

    Migrate: create system_logs

---
+++ database/migrations/2019_12_02_144033_create_system_logs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSystemLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('system_logs', function (Blueprint $table) {
+            $table->bigIncrements('log_id');
+            $table->tinyInteger('log_type');
+            $table->text('log_desc');
+            $table->timestamp('log_date')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('system_logs');
+    }
+}
