5d7711a1c3a8818ec85ab85ff197722fb2bb05ae
Earl Tacogdoy <earltacs96@gmail.com>
2019-12-02 22:35:22 +0800

    Migration: create user_groups

---
+++ database/migrations/2019_12_01_050837_create_user_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_groups', function (Blueprint $table) {
+            $table->Increments('group_id');
+            $table->string('group_name', 65);
+            $table->string('group_shortname', 11);
+            $table->timestamp('date_groupcreated')->useCurrent();
+            $table->timestamp('date_groupupdated')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_groups');
+    }
+}
