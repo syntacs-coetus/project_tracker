9f60062b84cfeb55a149e67f5e733668cc10ffef
Earl Tacogdoy <earltacs96@gmail.com>
2019-12-02 22:37:13 +0800

    Migration: create issue_milesone

---
+++ database/migrations/2019_12_02_142425_create_issue_milestone_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssueMilestoneTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issue_milestone', function (Blueprint $table) {
+            $table->bigIncrements('imilestone_id');
+            $table->Integer('milestone_id')->unsigned();
+            $table->Integer('issue_id')->unsigned();
+        });
+
+        Schema::table('issue_milstone', function (Blueprint $table){
+            $table->foreign('milestone_id')
+            ->references('milestone_id')
+            ->on('project_milestones')
+            ->onUpdate('cascade')->onDelete('restrict');
+
+            $table->foreign('issue_id')
+            ->references('issue_id')
+            ->on('project_issues')
+            ->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issue_milestone');
+    }
+}
