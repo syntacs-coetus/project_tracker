8a08d9c793c6933a786d402d754a63036b84f35a
Earl Tacogdoy <earltacs96@gmail.com>
2019-12-02 23:09:43 +0800

    Migrate: Added SystemAdministratorSeeder

---
+++ database/seeds/SystemAdministratorSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SystemAdministratorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user_accounts')->insert([
+            'user_name'           => 'AtraXaz',
+            'user_email'          => 'syntacs.2019@gmail.com',
+            'user_pass'           => make::hash('ShaneTacs2018'),
+            'user_administrator'  => 1,
+            'user_admin_override' => 1
+        ]);
+    }
+}
